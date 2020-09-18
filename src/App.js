cd3f1e4d53d9ff0f3cd3609226449d322c458191
Deepesh Rohit <rdeep6597@gmail.com>
2020-09-18 17:30:40 +0530

    evaluation

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+import './App.css';
+import Table from './component/Table';
+import Data from './component/Data'
+
+const App = ()=> { 
+  return (
+    <div className="App">
+    <Data></Data>
+    </div>
+  );
+}
+
+export default App;
